47ecee54b971d10b696c5f7a426fe256730610a3
Priyanthan <priyanthan2@gmail.com>
2022-06-30 17:52:09 +0530

    Implement Functions

--- Student Marks (OOP - Exersice).cpp
+++ Student Marks (OOP - Exersice).cpp
@@ -1,45 +1,110 @@
 
-
 #include <iostream>
-#include "string.h"
 
 using namespace std;
 
 class Result
 {
 private:
-	int Roll, M1, M2, M3, Total;
-	char name[25];
-
+	int Roll = 0;
+	int M1 = 0;
+	int M2 = 0;
+	int M3 = 0; 
+	int Total = 0;
+	string name;
 
 public:
 
 	// Accesors
-	char GetName();
+	int GetRollNumber();
+	int GetMarks();
+	string GetName();
+
 	
 
 	// Mutators
-	void SetName(char name[25]);
+	void SetRollNumber(int RN);
+	void SetMarks(int m1, int m2, int m3);
+	void SetName(string n);
 
 };
 
-char Result::GetName()
+// Getters
+int Result::GetRollNumber()
 {
-	return name[25];
+	return Roll;
 }
 
-void Result::SetName(char name[25])
+int Result::GetMarks()
 {
-	name[25] = this->name[25];
+	return M1, M2, M3;
+}
+
+string Result::GetName()
+{
+	return name;
+}
+
+// Setters
+
+void Result::SetRollNumber(int RN)
+{
+	Roll = RN;
+}
+
+void Result::SetMarks(int m1, int m2, int m3)
+{
+	M1 = m1;
+	M2 = m2;
+	M3 = m3;
+}
+
+void Result::SetName(string n)
+{
+	name = n;
 }
 
 int main()
 {
+	// Creating Object
 	Result r;
 
+	// Getting RollNumber
+	int RollN;
+	cout << "Enter Roll Number: " << endl;
+	cin >> RollN;
+	r.SetRollNumber(RollN);
+
+	// Set Name
+	string Name;
 	cout <<"Enter Name: " << endl;
-	cin << r.SetName();
+	cin >> Name;
+	r.SetName(Name);
 	
+	// Set Marks
+	int Marks1 = 0;
+	int Marks2 = 0;
+	int Marks3 = 0;
+	cout << " Enter Marks :" << endl;
+	for (int i = 1; i >= 3; i++)
+	{
+		cout << "Enter " << i << "Marks :" << endl;
+
+		if (Marks1 == 0 )
+		{
+			cin >> Marks1;
+		}
+		else if (Marks2 == 0)
+		{
+			cin >> Marks2;
+		}
+		else
+		{
+			cin >> Marks3;
+		}
+	}
+
+	cout << Marks1 << " " << Marks2 << " " << Marks3 << endl;
 	
 
 }
