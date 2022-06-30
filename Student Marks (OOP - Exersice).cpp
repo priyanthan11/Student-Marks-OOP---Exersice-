a68e82ced87988dad390e3d5df2cf81ef6f3c127
Priyanthan <priyanthan2@gmail.com>
2022-06-30 17:58:03 +0530

    More Functions
    -Setting Average

--- Student Marks (OOP - Exersice).cpp
+++ Student Marks (OOP - Exersice).cpp
@@ -11,6 +11,7 @@ private:
 	int M2 = 0;
 	int M3 = 0; 
 	int Total = 0;
+	int Average = 0;
 	string name;
 
 public:
@@ -19,6 +20,9 @@ public:
 	int GetRollNumber();
 	int GetMarks();
 	string GetName();
+	int GetTotal();
+	int GetAverage();
+
 
 	
 
@@ -26,6 +30,8 @@ public:
 	void SetRollNumber(int RN);
 	void SetMarks(int m1, int m2, int m3);
 	void SetName(string n);
+	void SetTotal(int X, int Y, int Z);
+	void SetAverage(int E, int F, int G);
 
 };
 
@@ -45,6 +51,15 @@ string Result::GetName()
 	return name;
 }
 
+int Result::GetTotal()
+{
+	return Total;
+}
+
+int Result::GetAverage()
+{
+	return Average;
+}
 // Setters
 
 void Result::SetRollNumber(int RN)
@@ -64,6 +79,21 @@ void Result::SetName(string n)
 	name = n;
 }
 
+void Result::SetTotal(int A, int B, int C)
+{
+	Total = A + B + C;
+}
+
+void Result::SetAverage(int E, int F, int G)
+{
+	Average = (E + F + G) / 3;
+}
+
+
+
+
+
+// Main
 int main()
 {
 	// Creating Object
@@ -85,26 +115,23 @@ int main()
 	int Marks1 = 0;
 	int Marks2 = 0;
 	int Marks3 = 0;
-	cout << " Enter Marks :" << endl;
-	for (int i = 1; i >= 3; i++)
-	{
-		cout << "Enter " << i << "Marks :" << endl;
-
-		if (Marks1 == 0 )
-		{
-			cin >> Marks1;
-		}
-		else if (Marks2 == 0)
-		{
-			cin >> Marks2;
-		}
-		else
-		{
-			cin >> Marks3;
-		}
-	}
-
-	cout << Marks1 << " " << Marks2 << " " << Marks3 << endl;
+	cout << " Enter 1st Marks :" << endl;
+	cin >> Marks1;
+	cout << " Enter 2nd Marks :" << endl;
+	cin >> Marks2;
+	cout << " Enter 3rd Marks :" << endl;
+	cin >> Marks3;
 	
+	r.SetMarks(Marks1, Marks2, Marks3);
+
+	// Set Total
+	r.SetTotal(Marks1, Marks2, Marks3);
+	//Get Total
+	r.GetTotal();
+
+	// SetAverage
+	r.SetAverage(Marks1, Marks2, Marks3);
+
+	// Setting Grade
 
 }
